945fde36a201384d8536bda9f47034ada95b0fb4
luka1zorko <luka1.zorko@gmail.com>
2021-01-06 17:37:39 +0100

    city list

---
+++ src/views/CitiesList.vue
@@ -0,0 +1,41 @@
+<template>
+  <div class="home">
+      <div v-if="cities.length == 0">No cities</div>
+      <div v-else>
+          <div v-for="(city, i) in cities" :key="i" @click="toWeather(city)">
+              {{city}}
+          </div>
+      </div>
+      <button>Add city</button>
+  </div>
+</template>
+
+<script>
+
+var cities = ["Novo mesto", "Ljubljana"]
+
+var toWeather = function (city) {
+    console.log(city);
+    this.$router.push({
+        name: 'CityWeather',
+        params: {
+            city
+        }
+    });
+}
+
+export default {
+  name: 'CitiesList',
+  data() {
+      return {
+          cities
+      } 
+  },
+  components: {
+
+  },
+  methods: {
+      toWeather
+  }
+}
+</script>
---
+++ src/views/CityWeather.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    {{city}} Weather
+  </div>
+</template>
+
+<script>
+
+export default {
+  name: 'CityWeather',
+  data() {
+      return {
+
+      } 
+  },
+  props: {
+    city: String
+  },
+  components: {
+
+  }
+}
+</script>
